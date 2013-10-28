4c9899fb80793f15dac170ec57b60aa3631588aa
wulx <gurdy.woo@gmail.com>
2013-10-28 09:05:46 +0800

    new funciton depth to calculate etch depth

---
+++ depth.m
@@ -0,0 +1,9 @@
+function etchDepth = depth(dwellTime, etchTime, f)
+%DEPTH distribution of etched depth
+
+% copyright (c) wulx, gurdy.woo@gmail.com
+% last modified by wulx, 2013/10/28
+
+% TODO -- there are many factors to affect the final etch depth actually 
+% and here f is taken as the linear factor representing all others.
+etchDepth = f * (etchTime - dwellTime);
--- dwellTest.m
+++ dwellTest.m
@@ -31,9 +31,15 @@ leafWidth = 60; % in mm
 initAngleDeg = 90;
 projWidths = step2width(steps, stepAngleDeg, leafWidth, initAngleDeg);
 
-scaleDivs = 0:200;
+scaleDivs = 0:300;
 [dwellTime, vLeaf] = dwell(projWidths, timeline, scaleDivs);
 
 figure, plot(scaleDivs, dwellTime);
 title(['translational velocity: ', num2str(vLeaf), ' ; number of steps: ', num2str(numel(timeSeqs))]);
+
+f = 1;
+etchTime = timeline(end) - timeline(1);
+etchDepth = depth(dwellTime, etchTime, f);
+
+figure, plot(scaleDivs, etchDepth);
 
